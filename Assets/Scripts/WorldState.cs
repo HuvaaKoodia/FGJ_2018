652506708592e367f7e02dda7187904690e39868
Antti Harju <antti.harju@saunalahti.fi>
2018-01-27 23:59:21 +0200

    World state component, GM prefab and some references

---
+++ Assets/Scripts/WorldState.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldState : MonoBehaviour {
+
+	public static bool ServerOpen1;
+	public static bool ServerOpen2;
+	public static bool ServerOpen3;
+
+}
