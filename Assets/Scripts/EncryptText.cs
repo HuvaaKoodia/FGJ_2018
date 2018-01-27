59e1f924f903f11c09d038eccf3d6120d4d91727
Antti Harju <antti.harju@saunalahti.fi>
2018-01-27 17:21:35 +0200

    Kool encryption animation

--- Assets/Scripts/EncryptText.cs
+++ Assets/Scripts/EncryptText.cs
@@ -9,6 +9,9 @@ public class EncryptText : MonoBehaviour {
 
 	public InputField input;
 	public InputField output;
+	public Animator waveAnimation;
+
+	float timer;
 
 	// Use this for initialization
 	void Start () {
@@ -17,10 +20,17 @@ public class EncryptText : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(timer > 0) {
+			timer -= Time.deltaTime;
+			if(timer < 0) {
+				timer = 0;
+				OutputHash();
+				waveAnimation.SetBool("Wave", false);
+			}
+		} 
 	}
 
-	public void Enrypt() {
+	void OutputHash() {
 		// Super secure MD5 hash
 		MD5 md5hash = MD5.Create(); 
 		byte[] data = md5hash.ComputeHash(Encoding.UTF8.GetBytes(input.text.Trim()));
@@ -29,5 +39,11 @@ public class EncryptText : MonoBehaviour {
 			sb.Append(data[i].ToString("x2"));
 		}
 		output.text = sb.ToString();
+	}
+
+	public void Enrypt() {
+		timer = 1f;
+		output.text = "";
+		waveAnimation.SetBool("Wave", true);
 	}
 }
