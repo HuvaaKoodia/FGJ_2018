1de8251c8978efe06f9f21a6c247c232188c71e4
Antti Harju <antti.harju@saunalahti.fi>
2018-01-27 15:25:12 +0200

    Started Super Secure Text Encrypter

---
+++ Assets/Scripts/EncryptText.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EncryptText : MonoBehaviour {
+
+	public InputField input;
+	public InputField output;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Enrypt() {
+		// Super secure MD5 hash
+		MD5 md5hash = MD5.Create(); 
+		byte[] data = md5hash.ComputeHash(Encoding.UTF8.GetBytes(input.text.Trim()));
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < data.Length; i++) {
+			sb.Append(data[i].ToString("x2"));
+		}
+		output.text = sb.ToString();
+	}
+}
