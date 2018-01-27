24d61c5aa3e0e236334d5f5670b7792c1d2416b4
Arttu Knuutinen <arttu.knuutinen@gmail.com>
2018-01-27 19:02:21 +0200

    Password cracker readyish

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class Clock : MonoBehaviour {
+    Text clock;
+    int currentHour, currentMinute;
+
+    //public UnityEvent 
+	void Start () {
+        clock = GetComponentInChildren<Text>();
+        UpdateTime();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(System.DateTime.Now.Minute != currentMinute) {
+            UpdateTime();
+        }
+	}
+    void UpdateTime() {
+        currentHour = System.DateTime.Now.Hour;
+        currentMinute = System.DateTime.Now.Minute;
+        string AMPM = currentHour < 12 ? " AM" : " PM";
+        clock.text = currentHour.ToString() + ":" + currentMinute.ToString() + AMPM;
+    }
+}
---
+++ Assets/Scripts/PasswordCracker.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PasswordCracker : MonoBehaviour {
+    public InputField crackField, outputField;
+    public List<string> hashes = new List<string>();
+    public List<string> passwords = new List<string>();
+    public Scrollbar loadingbar;
+    public Text log;
+
+    public void SolveHash() {
+        StartCoroutine(ProgressBar(5f));
+        log.text = "";
+        outputField.text = "";
+
+    }
+    int CheckForValidHash(string hash) {
+        for (int i = 0; i < hashes.Count; i++) {
+            if (hashes[i].Contains(hash)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    IEnumerator ProgressBar(float loadTime) {
+        float t = 0f;
+        while (t < loadTime) {
+            t += Time.deltaTime;
+            loadingbar.size = t / loadTime;
+            yield return null;
+        }
+        int i = CheckForValidHash(crackField.text);
+        if (i < 0) {
+            log.text = "Error";
+        } else {
+            outputField.text = passwords[i];
+            log.text = "Success!";
+        }
+        crackField.text = "";
+        loadingbar.size = 0;
+    }
+}
