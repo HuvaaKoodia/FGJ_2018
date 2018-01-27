53df86d95146abfde84e28d595c6c49609b10a92
Arttu Knuutinen <arttu.knuutinen@gmail.com>
2018-01-27 14:59:07 +0200

    Inputfield prefab, passwordcracker WIP

---
+++ Assets/PasswordCracker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PasswordCracker : MonoBehaviour {
+    public InputField crackField, outputField;
+    public List<string> hashes = new List<string>();
+
+    public void SolveHash() {
+        if (!CheckForValidHash(crackField.text)) {
+            print("Not a valid hash");
+        } else {
+            print("hash found");
+            outputField.text = "password";
+        }
+        crackField.text = "";
+    }
+    bool CheckForValidHash(string hash) {
+        for (int i = 0; i < hashes.Count; i++) {
+            if (hashes[i].Contains(hash)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
