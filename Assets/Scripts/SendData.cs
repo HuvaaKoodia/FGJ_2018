fb3846076ce0678a4b78331a39f78ddeeace62dd
Antti Harju <antti.harju@saunalahti.fi>
2018-01-27 21:12:09 +0200

    Start SFTP logic and add sysicons to references

---
+++ Assets/Scripts/SendData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SendData : MonoBehaviour {
+
+	public InputField dataInput;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Send() {
+		print("Send data: " + dataInput.text);
+	}
+}
