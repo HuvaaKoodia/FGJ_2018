04e47837dc64c38a65d641711b0a7cbbf446005d
Antti Harju <antti.harju@saunalahti.fi>
2018-01-28 14:56:56 +0200

    SFTPPipe transmission window logic

--- Assets/Scripts/SendData.cs
+++ Assets/Scripts/SendData.cs
@@ -41,59 +41,99 @@ public class SendData : MonoBehaviour {
 	}
 
 	void Connect() {
-		if(WorldState.ServerOpen1 && serverIndex == 1 && server1.IsInteractable()) {
+
+		// Debug
+		WorldState.ServerOpen1 = true;
+		WorldState.ServerOpen2 = true;
+		WorldState.ServerOpen3 = true;
+
+		var error = "Connection failed.";
+
+		if(serverIndex == 1 && server1.IsInteractable()) {
 			networkText.text += "\nConnecting to " + server1.text;
+			if(!WorldState.ServerOpen1) {
+				validConnection = false;
+				timer = 0;
+				error = "Access denied.";
+				goto Check;
+			}
+			if(server1.text.Trim() != WorldState.server1_1Address) {
+				validConnection = false;
+				timer = 0;
+				error = "Address not found.";
+				goto Check;
+			}
 			timer += CONNECTION_TIME;
 			return;
-		} else if(!WorldState.ServerOpen1 && serverIndex == 1 && server1.IsInteractable()) {
-			validConnection = false;
-			timer += 1f;
-		} else if (serverIndex == 1) {
+		} else if(serverIndex == 1) {
 			networkText.text += "\nLink 1 not defined";
 			timer += 1f;
 		}
-		if(WorldState.ServerOpen2 && serverIndex == 2 && server2.IsInteractable()) {
+		if(serverIndex == 2 && server2.IsInteractable()) {
 			networkText.text += "\nConnecting to " + server2.text;
+			if(!WorldState.ServerOpen2) {
+				validConnection = false;
+				timer = 0;
+				error = "Access denied.";
+				goto Check;
+			}
+			if(server2.text.Trim() != WorldState.server2_1Address) {
+				validConnection = false;
+				timer = 0;
+				error = "Address not found.";
+				goto Check;
+			}
 			timer += CONNECTION_TIME;
 			return;
-		} else if(!WorldState.ServerOpen2 && serverIndex == 2 && server2.IsInteractable()) {
-			validConnection = false;
-			timer += 1f;
-		} else if (serverIndex == 2){
+		} else if(serverIndex == 2) {
 			networkText.text += "\nLink 2 not defined";
 			timer += 1f;
 		}
-		if(WorldState.ServerOpen3 && serverIndex == 3 && server3.IsInteractable()) {
+
+		if(serverIndex == 3 && server3.IsInteractable()) {
 			networkText.text += "\nConnecting to " + server3.text;
+			if(!WorldState.ServerOpen3) {
+				validConnection = false;
+				timer = 0;
+				error = "Access denied.";
+				goto Check;
+			}
+			if(server3.text.Trim() != WorldState.server3_1Address) {
+				validConnection = false;
+				timer = 0;
+				error = "Address not found.";
+				goto Check;
+			}
 			timer += CONNECTION_TIME;
 			return;
-		} else if(!WorldState.ServerOpen3 && serverIndex == 3 && server3.IsInteractable()) {
-			validConnection = false;
-			timer += 1f;
-		} else if (serverIndex == 3){
+		} else if(serverIndex == 3) {
 			networkText.text += "\nLink 3 not defined";
 			timer += 1f;
 		}
-		if(serverIndex == 4 && server4.IsInteractable()) {
+		// End
+		if(serverIndex == 4 && server4.text.Trim() == WorldState.server4Address) {
 			networkText.text += "\nConnecting to " + server4.text;
 			timer += CONNECTION_TIME;
 			return;
-		} else if(serverIndex == 4 && !server1.IsInteractable()) {
+		} else if(serverIndex == 4 && server4.text.Trim() != WorldState.server4Address) {
 			validConnection = false;
-			timer += 1f;
-		} else if (serverIndex == 4){
-			networkText.text += "\nTarget Link not defined";
+			error = "Address not found.";
+			timer = 0;
+		} else if(serverIndex == 4) {
+			Debug.LogWarning("Should not happen!");
 			timer += 1f;
 		}
+
+		Check:
 
 		if(validConnection && serverIndex == 5) {
 			sound.dial.Stop();
 			transmissionWindow.Open();
 			print("Send data: " + dataInput.text);
-		} else if(serverIndex == 5) {
+		} else if(!validConnection) {
 			timer = 0;
 			networkWindow.Close();
-			warning.Open("Invalid pipe.\nAccess denied to link server.");
+			warning.Open("Invalid pipe.\n" + error);
 		}
 
 	}
@@ -101,7 +141,7 @@ public class SendData : MonoBehaviour {
 	public void Send() {
 
 		// End server must be defined
-		if(!server4.IsInteractable()) {
+		if(server4.text.Trim() == "") {
 			warning.Open("Invalid pipe.\nTarget not defined.");
 			networkWindow.Close();
 		} else {
