b821b0b6dde388b36dab6a18670fbf6ae41b21b7
HuvaaKoodia <huvaakoodia@gmail.com>
2018-01-28 16:49:30 +0200

    quick and dirty ending panel

--- Assets/Scripts/GUI/EndingPanel.cs
+++ Assets/Scripts/GUI/EndingPanel.cs
@@ -1,27 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class EndingPanel : MonoBehaviour 
 {
 #region variables
 	public SendData sendData;
+	public CanvasGroup fader;
+	public GameObject inputBlock;
+	public Text successText;
 #endregion
 #region initialization
-	private void Start () 
-	{
-
-	}
 #endregion
 #region logic
-	private void Update () 
-	{
-	
-	}
 #endregion
 #region public interface
 	public void ShowEndingTransmit(bool transmissionEnded)
 	{
+		inputBlock.SetActive(true);
+		StartCoroutine(FadeToBlack(0.5f));
+
 		if(transmissionEnded)
 		{
 			
@@ -33,6 +32,18 @@ public class EndingPanel : MonoBehaviour
 	}
 #endregion
 #region private interface
+
+	IEnumerator FadeToBlack(float s)
+	{
+		while(fader.alpha < 1)
+		{
+			yield return null;
+			fader.alpha += s * Time.deltaTime;
+		}
+
+		fader.alpha = 1f;
+		successText.enabled = true;
+	}
 #endregion
 #region events
 #endregion
